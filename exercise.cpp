20b26d2d5b5bfce99753990a6534c4ea85ead3d9
yasinkuyuk <yasin.kuyuk@albert.health>
2020-12-06 21:21:20 +0300

    implementation finished without 'e' notation

--- exercise.cpp
+++ exercise.cpp
@@ -1,20 +1,85 @@
 #include <iostream>
 #include <time.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void quicksort(string[]);
+void quicksort(string[],int,int);
+int divide(string[],int,int);
+bool alphabetPresedence(string,string);
 
 int main(){
-    string strList[10] = {"selam","bana","nasıl","kelime","hediye","sakal","ceviz","kolye","önlük","cam"};
+    string strList[10] = {"selam","bana","nasil","kelime","hediye","sakal","ceviz","kolye","onluk","cam"};
+    //int arr[10] = {3,92,35,2,78,5,61,72,88,68};
 
-    quicksort(strList);
+    quicksort(strList,0,9);
+    //quicksort(arr,0,9);
 
-    cout<<strList[5];
+    for(int i=0; i<10; i++){
+        //cout << arr[i]<<" ";
+        cout << strList[i]<<" ";
+    }
 
     return EXIT_SUCCESS;
 }
 
-void quicksort(string[]){
-    
+bool alphabetPresedence(string first,string second){ //does first string comes after from second string func
+    int firstLength = first.length(); //5
+    int secondLength = second.length(); //7
+    int minimum = min(firstLength,secondLength);
+    bool temp = false;
+
+    for(int i=0; i<minimum; i++){ //[0,5)
+        if(first[i] == second[i]){
+            if(i == minimum-1 && firstLength == secondLength){
+                if(firstLength > secondLength){
+                    temp = true;
+                }
+                break;
+            }
+            continue;
+        }
+
+        if(first[i] > second[i]){
+            temp = true;
+            break;
+        }
+        else{
+            break;
+        }
+    }
+    return temp;
 }
+
+int divide(string arr[], int start, int end){
+    string pivot = arr[start];
+    //string pivot =arr[start];
+
+    int i = start;
+
+    for(int j = start+1; j<=end; j++ ){
+        if(alphabetPresedence(arr[j],pivot)){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+        /* if(pivot > arr[j]){
+            i++;
+            swap(arr[i],arr[j]);
+        } */
+    }
+
+    swap(arr[start], arr[i]);
+
+    return i;
+}
+
+void quicksort(string arr[],int start,int end){
+    if(start < end){
+        int pivot = divide(arr,start,end);
+
+        quicksort(arr,start,pivot-1);
+        quicksort(arr,pivot+1,end);
+    }
+}
+ 
+
--- proj.cpp
+++ proj.cpp
@@ -3,8 +3,13 @@
 #include<string>
 #include <iostream> 
 #include <sstream> 
+#include <chrono> 
+#include<vector> 
+#include <bits/stdc++.h>
+#include <ctime>
 
 using namespace std;
+using namespace std::chrono;
 
 class Sale{
 	string country;
@@ -52,54 +57,142 @@ class Sale{
 		double getTotalProfit(){
 			return this->totalProfit;
 		}
+		
 };
 
+bool alphabetPresedence(string first,string second){ //does first string comes after from second string func
+    int firstLength = first.length(); //5
+    int secondLength = second.length(); //7
+    int minimum = min(firstLength,secondLength);
+    bool temp = false;
 
-int main(){
-	
+    for(int i=0; i<minimum; i++){ //[0,5)
+        if(first[i] == second[i]){
+            if(i == minimum-1 && firstLength == secondLength){
+                if(firstLength > secondLength){
+                    temp = true;
+                }
+                break;
+            }
+            continue;
+        }
+
+        if(first[i] > second[i]){
+            temp = true;
+            break;
+        }
+        else{
+            break;
+        }
+    }
+    return temp;
+}
+
+int divide(Sale arr[], int start, int end){
+    string pivot = arr[start].getCountry();
+	double pivotTotal = arr[start].getTotalProfit();
+
+    int i = start;
+
+    for(int j = start+1; j<=end; j++ ){
+		if(arr[j].getCountry() == pivot){ //sorting same country names dsecending total profit
+			if(pivotTotal < arr[j].getTotalProfit()){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+		}
+        if(alphabetPresedence(arr[j].getCountry(),pivot)){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+
+    swap(arr[start], arr[i]);
+
+    return i;
+}
+
+void quicksort(Sale arr[],int start,int end){
+    if(start < end){
+        int pivot = divide(arr,start,end);
+
+        quicksort(arr,start,pivot-1);
+        quicksort(arr,pivot+1,end);
+    }
+}
+
+void readFromFile(Sale arr[],string filename,int lineNumber){
 	ifstream file;
-	file.open("sales.txt");
+	file.open(filename);
 	
 	if (!file){
 		cerr << "File cannot be opened!";
 		exit(1);
 	}
 
-		
-	int N = 10; //array size
 	string line;
 
-	Sale salesArray[N];
-	
 	getline(file, line); //this is the header line
-
+	
 	int tempUnitSold = 0;
 	double tempTotalProfit = 0.0;
 
-	for(int i = 0; i<N; i++){ //creating sales array
+	for(int i = 0; i<lineNumber; i++){ //creating sales array
 		getline(file, line, '\t'); //country (string)
-		salesArray[i].setCountry(line);
+		arr[i].setCountry(line);
 
 		getline(file, line, '\t'); //item type (string)
-		salesArray[i].setItemType(line);
+		arr[i].setItemType(line);
 
 		getline(file, line, '\t'); //order id (string)
-		salesArray[i].setOrderId(line);
+		arr[i].setOrderId(line);
 
 		file >> line; //units sold (integer)
 		stringstream converter(line);
 		converter >> tempUnitSold;
-		salesArray[i].setUnitsSold(tempUnitSold);
+		arr[i].setUnitsSold(tempUnitSold);
 
 		file >> line; //total profit (float)
 		stringstream converter2(line);
 		converter2 >> tempTotalProfit;
-		salesArray[i].setTotalProfit(tempTotalProfit);
+		arr[i].setTotalProfit(tempTotalProfit);
 
 	    getline(file, line, '\n'); //this is for reading the \n character into dummy variable.
-
 	}
+}
 
+void writeToFile(Sale arr[],string filename,int lineNumber){
+	ofstream myfile;
+	myfile.open(filename);
+	myfile << "Country" << "\t" << "Item Type" << "\t" << "Order ID" << "\t" << "Units Sold" << "\t" << "Total Profit" << endl;
+	
+	for (int i = 0; i < lineNumber; i++) {
+		myfile << arr[i].getCountry() << "\t" << arr[i].getItemType() << "\t"<< arr[i].getOrderId() <<"\t" 
+		<< arr[i].getUnitsSold()<< "\t"<< arr[i].getTotalProfit() << endl;
+	}
+}
+
+int main(int argc, char* argv[]){
+	//assignments
+	string readingfile="sales.txt";
+	string writingFilename="sorted.txt";
+	int N;
+	stringstream converter(argv[1]);
+	converter >> N;
+	Sale* salesArray = new Sale[N];
+
+	readFromFile(salesArray,readingfile,N); //reads file and stores the data in Sale array
+
+	clock_t startTime= clock(); //starting time of the sorting algorithm
+	quicksort(salesArray,0,N-1); //sorting the Sale array descending alphabetic order,total profit
+	clock_t endTime = clock();
+	clock_t t = endTime - startTime; //finishing time of the sorting algorithm
+	
+	writeToFile(salesArray,writingFilename,N); //writes the sorted Sale array to the file
+
+	cout<<"N: "<<N<<" Time: "<<(float)t/CLOCKS_PER_SEC<<endl; //printing the algorithm execution time on console
 	return EXIT_SUCCESS;
 }
+
+
 
